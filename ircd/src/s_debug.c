ec72d2c68e48ee8269ea105d58400529e5898e55
skandranon <>
2003-11-24 02:52:48 +0000

    use strerror() not sys_errlist[]

--- ircd/src/s_debug.c
+++ ircd/src/s_debug.c
@@ -127,11 +127,8 @@ void	send_usage(aClient *cptr, char *nick)
 
 	if (getrusage(RUSAGE_SELF, &rus) == -1)
 	    {
-#if !defined(__FreeBSD__) && !defined(__NetBSD__)
-		/* extern char *sys_errlist[]; */
-#endif
 		sendto_one(cptr,":%s NOTICE %s :Getruseage error: %s.",
-			   me.name, nick, sys_errlist[errno]);
+			   me.name, nick, strerror(errno));
 		return;
 	    }
 	secs = rus.ru_utime.tv_sec + rus.ru_stime.tv_sec;
