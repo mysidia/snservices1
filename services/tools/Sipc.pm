abba9711334c76bcda4129821c24b8bc1d30cfe9
Mysidia <69487+mysidia@users.noreply.github.com>
2004-01-15 03:56:03 +0000

    now support querying all public nick info

--- services/tools/Sipc.pm
+++ services/tools/Sipc.pm
@@ -17,6 +17,39 @@ sub Errmsg
 	return $SIPC::ERRORTEXT;
 }
 
+
+sub getPublicNickInfo
+{
+	my $n = {}, $o;
+	my $self = shift;
+	my $target = shift;
+	
+	(($n->{NICK} = $self->queryNick($target, "")) ne undef) or return undef;
+	(($n->{REGTIME} = $self->queryNick($target, "TIMEREG")) ne undef) or return undef;
+	(($n->{URL} = $self->queryNick($target, "URL")) ne undef) or return undef;
+	(($n->{FLAGS} = $self->queryNick($target, "FLAGS")) ne undef) or return undef;
+	(($n->{BADPWS} = $self->queryNick($target, "BADPWS")) ne undef) or return undef;
+	(($n->{'LAST-TIME'} = $self->queryNick($target, "LAST-TIME")) ne undef) or return undef;
+	(($n->{ACC} = $self->queryNick($target, "ACC")) ne undef) or return undef;
+	(($n->{MARK} = $self->queryNick($target, "MARK")) ne undef) or return undef;
+	(($n->{'LAST-HOST'} = $self->queryNick($target, "LAST-HOST")) ne undef) or return undef;
+	return $n;
+}
+
+sub getPrivateNickInfo
+{
+        my $n = {};
+        my $self = shift;
+        my $target = shift;
+
+	$n->{'LAST-HOST-UNMASK'} = $self->queryNick($target, "LAST-HOST-UNMASK") or return undef;
+	$n->{EMAIL} = $self->queryNick($target, "EMAIL") or return undef;
+	(($n->{OPFLAGS} = $self->queryNick($target, "OPFLAGS")) ne undef) or return undef;
+	(($n->{NUM_MASKS} = $self->queryNick($target, "NUM-MASKS")) ne undef) or return undef;
+	$n->{'IS-READTIME'} = $self->queryNick($target, "IS-READTIME") or return undef;
+
+}
+
 sub queryNick
 {
 	my $self = shift;
@@ -25,31 +58,49 @@ sub queryNick
 	my $sock = $self->{sock};
 	my $sel = $self->{sel};
 
-	#print "\nQUERY OBJECT RNICK " . $qtarget . " " . $qfield . "\n";
 	$sock->print("QUERY OBJECT RNICK " . $qtarget . " " . $qfield . "\n");
+
 	for(;;) {
 		for ($sel->can_read(5)) {
 			if ($_ ne $self->{sock}) {
+				$SIPC::ERRORTEXT = "Wrong socket\n";
 				return undef;
 			}
 			while ($line = $sock->getline) {
 				#print "\n" . $line . "\n";
 				if ($line =~ /^ERR-BADTARGET QUERY OBJECT RNICK=(\S+) - (.*)/)
 				{
-					$SIPC::ERRORTEXT = $2;
+					$SIPC::ERRORTEXT = "Prob: " . $2 . "\n";
 					return undef;
 				}
 				elsif ($line =~ /^ERR-BADATT QUERY OBJECT RNICK EATTR - (.*)/) 
 				{
+
 					$SIPC::ERRORTEXT = "Unknown attribute: RNICK " . $qfield;
 					return undef;
+				}
+				elsif ($line =~ /^OK QUERY OBJECT RNICK=(\S+) ISREG=(\S+) - .*/) {
+					my $m_nick = $1;
+					my $m_isreg = $2;
+
+					if ($qfield ne "") {
+						return $m_isreg;
+					}
+					else {
+						if ($m_isreg eq "TRUE") {
+							return $m_nick;
+						}
+						else {
+							return undef;
+						}
+					}
 				}
 				elsif ($line =~ /^OK QUERY OBJECT RNICK=(\S+) (\S+) (.*)/)
 				{
 					if ($1 ne $qtarget) {
 						warn "$1 <> $target" . "\n";
 					}
-					return $3;
+					return ($1, $3);
 				}
 			}
 		}
